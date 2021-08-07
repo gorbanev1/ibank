10fa0ab493e420bc0ddf01d3aeb8492d7489a3eb
gorbanevi <gorbanev_i@mail.ru>
2021-08-07 23:24:21 +0300

    final tests passed

--- js/app.js
+++ js/app.js
@@ -2,11 +2,11 @@
 
 const API_URL = 'http://localhost:9999/api';
 const showLoader = function (amountEl, nameEl, numberEl) {
-    amountEl.innerHTML = `<div class="loading-indicator"></div>`;
-    nameEl.innerHTML = `<div class="loading-indicator"></div>`;
-    numberEl.innerHTML = `<div class="loading-indicator"></div>`;
+    amountEl.innerHTML = '<div class="loading-indicator"></div>';
+    nameEl.innerHTML = '<div class="loading-indicator"></div>';
+    numberEl.innerHTML = '<div class="loading-indicator"></div>';
 };
-const showAccount = (amountEl,  numberEl, nameEl, data, err) => {
+const showAccount = (amountEl, numberEl, nameEl, data, err) => {
     if (err){
         amountEl.innerHTML=`
         <div>Произошла ошибка</div>
@@ -14,7 +14,7 @@ const showAccount = (amountEl,  numberEl, nameEl, data, err) => {
         return;
     }
     // amountEl.innerHTML=`<div>${data.account.amount.toFixed(2)} руб.</div>`;
-    amountEl.innerHTML=`<div>${String(data.account.amount).replace('.',',')} руб.</div>`;
+    amountEl.innerHTML=`<div>${String(data.account.amount).replace('.',',')}</div>`;
     //amountEl.innerHTML=`<div>${data.account.amount.replace(/./,",")} руб.</div>`;
     numberEl.innerHTML=`<div>${data.account.number}</div>`;
     nameEl.innerHTML=`<div>${data.account.name}</div>`;
@@ -31,62 +31,9 @@ const loadAccount = async (elAmm, elNmb, elName)=> {
         }
         const data=await response.json();
         showAccount(elAmm, elNmb, elName, data);
-    }    catch (e){
+    } catch (e){
         console.error(e);
         showAccount(elAmm, elNmb, elName, null, e);
     }
 };
 loadAccount(amountEl,numberEl, nameEl);
-
-
-
-
-// const request = (method, url)=> {
-//     return new Promise((resolve, reject) => {
-//         const xhr = new XMLHttpRequest();
-//         xhr.open(method, url);
-//         xhr.onload = (evt) => resolve(evt);
-//         xhr.onerror = (evt) => reject(evt);
-//         xhr.send();
-//     })
-// }
-// showLoader(bonusesEl);
-//
-// fetch(`${API_URL}/lection/slow/bonuses`)
-//     .then((response)=>{
-//         if (!response.ok) {
-//             throw new Error('err.server');
-//         }
-//         return response.json();
-//         })
-//     .then((data)=>{
-//         showBonuses(bonusesEl, data);
-//     })
-//     .catch((error)=>{
-//         showBonuses(bonusesEl, null, 'err.common');
-//     });
-//
-// request('GET', `${API_URL}/lection/bonuses/error`)
-//     .then((evt)=>{
-//             alert('sfsdfsa');
-//             if (evt.target.status!==200){
-//                 //showBonuses(bonusesEl, null, 'err.server');
-//                 throw new Error('err.server');
-//                 alert('fsfasdfsadfasdas');
-//                  return;
-//             }
-//             const data =JSON.parse(evt.target.responseText);
-//             showBonuses(bonusesEl, data);
-//         })
-//     .catch(()=>{
-//             showBonuses(bonusesEl, null, 'err.network');
-//             });
-
-
-
-// document.body.addEventListener('click',(evt)=>console.log('clicked'));
-// const response = xhr.responseText;
-// console.log(response);
-// setTimeout(()=>{
-//
-// },0);
