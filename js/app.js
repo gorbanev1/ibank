9321a60296dddb8fc3d2cdfef58cbfcd148287dd
gorbanevi <gorbanev_i@mail.ru>
2021-08-08 11:48:31 +0300

    tests passed dirty code

--- js/app.js
+++ js/app.js
@@ -6,13 +6,39 @@ const showLoader = function (amountEl, nameEl, numberEl) {
     nameEl.innerHTML = '<div class="loading-indicator"></div>';
     numberEl.innerHTML = '<div class="loading-indicator"></div>';
 };
-const showAccount = (amountEl, numberEl, nameEl, data, err) => {
+const showAccount = (amountEl, numberEl, nameEl, infoEl, data, err) => {
     if (err){
-        amountEl.innerHTML=`
-        <div>Произошла ошибка</div>
+        //alert('dddddddddddddddddddddddddddddddd');
+        infoEl.innerHTML=`
+        Произошла ошибка
+        <button class="retry">Повторить запрос</button>
         `;
+        const retryButton = document.querySelector('.retry');
+
+        retryButton.onclick=()=>{
+            infoEl.innerHTML='';
+            infoEl.appendChild(nameEl);
+            infoEl.appendChild(numberEl);
+            const divBalance=document.createElement('div');//.setAttribute("class","balance");
+            //alert(divBalance);
+            divBalance.setAttribute('class','balance');
+            divBalance.appendChild(amountEl);
+            divBalance.appendChild(document.createTextNode(' ₽'));
+            infoEl.appendChild(divBalance);
+            loadAccount(amountEl,numberEl, nameEl, infoEl);
+        };
+
+
         return;
     }
+
+    // infoEl.innerHTML=`
+    //         <div class="name">Текущий счет</div>
+    //         <div class="number">xxxxx55555555555xxxxx</div>
+    //         <div class="balance">
+    //             <span class="amount">999,99</span> ₽
+    //         </div>
+    //     `;
     amountEl.textContent=`${String(data.account.amount).replace('.',',')}`;
     numberEl.textContent=`${data.account.number}`;
     nameEl.textContent=`${data.account.name}`;
@@ -20,18 +46,24 @@ const showAccount = (amountEl, numberEl, nameEl, data, err) => {
 const amountEl =document.querySelector('.amount');
 const nameEl = document.querySelector('.name');
 const numberEl = document.querySelector('.number');
-const loadAccount = async (elAmm, elNmb, elName)=> {
+const infoEl = document.querySelector('.info');
+const loadAccount = async (elAmm, elNmb, elName, elInfo)=> {
+    showLoader(elAmm, elNmb, elName);
     try {
-        showLoader(elAmm, elNmb, elName);
-        const response = await fetch (`${API_URL}/hw15`);
+        const response = await fetch (`${API_URL}/hw16`);
         if (!response.ok){
+            //alert(response.statusText);
             throw new Error((response.statusText));
+
         }
+        //alert('try');
         const data=await response.json();
-        showAccount(elAmm, elNmb, elName, data);
+        showAccount(elAmm, elNmb, elName, elInfo, data);
     } catch (e){
         console.error(e);
-        showAccount(elAmm, elNmb, elName, null, e);
+        //alert(e);
+        showAccount(elAmm, elNmb, elName, elInfo, null, e);
+
     }
 };
-loadAccount(amountEl,numberEl, nameEl);
+loadAccount(amountEl,numberEl, nameEl, infoEl);
